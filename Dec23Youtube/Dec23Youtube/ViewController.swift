e2022c0ce67f240477feae72edf1f4ec9a55a745
NiNajamie <NiNajamie@users.noreply.github.com>
2016-02-21 00:08:27 -0800

    Added files via upload

---
+++ Dec23Youtube/Dec23Youtube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dec23Youtube
+//
+//  Created by Asuka Nakagawa on 2015-12-23.
+//  Copyright © 2015 Asuka Nakagawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
